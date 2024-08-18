adcc721c781b878b58c5ad0d1972224a8f004c66
unknown <manarkhaled2510@gmail.com>
2024-08-18 16:08:11 +0300

    resposive day 1

---
+++ JavaScript/day1/script.js
@@ -0,0 +1,21 @@
+/*************task1***************/
+
+// alert('welcome to my site')
+
+
+
+// let name=prompt('enter your name')
+
+// document.write(`welcome, ${name}`)
+/*************task2***************/
+// let x=prompt('enter x')
+// let y=prompt('enter y')
+
+// function add(a,b){
+// return a+b;
+// }
+
+// console.log(add(x,y))
+
+
+/*************task3***************/
